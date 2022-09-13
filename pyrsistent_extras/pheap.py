6ee27931d7f92d68c092057b4d7bbe594f86dcd1
mingmingrr <mingmingrr@gmail.com>
2022-09-13 11:07:46 -0400

    heap: cleanup types

--- pyrsistent_extras/pheap.py
+++ pyrsistent_extras/pheap.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import ClassVar, TypeVar, Generic, Optional, Any, Union, cast
-from collections.abc import Collection, Iterable, Iterator
+from typing import Collection, Iterable, Iterator, \
+	ClassVar, TypeVar, Generic, Optional, Tuple, Any, Union, cast
 from abc import abstractmethod
 
 from .util import Comparable
@@ -11,18 +10,25 @@ T = TypeVar('T')
 K = TypeVar('K', bound=Comparable)
 V = TypeVar('V')
 
-@dataclass(slots=True, frozen=True)
 class Branch(Generic[K,V]):
+	__slots__ = ('tree', 'next')
+
 	tree: Tree[K,V]
 	next: Optional[Branch[K,V]]
 
-	def check(self:Optional[Branch[K,V]], key:K, order:int) -> int:
+	def __new__(cls, tree:Tree[K,V], next:Optional[Branch[K,V]]):
+		self = super().__new__(cls)
+		self.tree = tree
+		self.next = next
+		return self
+
+	def _check(self:Optional[Branch[K,V]], key:K, order:int) -> int:
 		if self is None:
 			assert order == -1
 			return 0
 		else:
-			tsize = self.tree.check(key, order)
-			bsize = Branch.check(self.next, key, order - 1)
+			tsize = self.tree._check(key, order)
+			bsize = Branch._check(self.next, key, order - 1)
 			return tsize + bsize
 
 	def pprint(self:Optional[Branch[K,V]], indent:int):
@@ -30,16 +36,24 @@ class Branch(Generic[K,V]):
 		self.tree.pprint(indent)
 		Branch.pprint(self.next, indent)
 
-@dataclass(slots=True, frozen=True)
 class Tree(Generic[K,V]):
+	__slots__ = ('key', 'value', 'branch')
+
 	key: K
 	value: V
 	branch: Optional[Branch[K,V]]
 
-	def check(self, key:K, order:int) -> int:
+	def __new__(cls, key:K, value:V, branch:Optional[Branch[K,V]]):
+		self = super().__new__(cls)
+		self.key = key
+		self.value = value
+		self.branch = branch
+		return self
+
+	def _check(self, key:K, order:int) -> int:
 		assert self.key >= key
 		assert self.order() == order
-		size = Branch.check(self.branch, self.key, order - 1)
+		size = Branch._check(self.branch, self.key, order - 1)
 		return size + 1
 
 	def merge(self, other:Optional[Tree[K,V]]) -> Tree[K,V]:
@@ -60,16 +74,24 @@ class Tree(Generic[K,V]):
 			order, branch = order + 1, branch.next
 		return order
 
-@dataclass(slots=True, frozen=True)
 class Forest(Generic[K,V]):
+	__slots__ = ('order', 'tree', 'next')
+
 	order: int
 	tree: Tree[K,V]
 	next: Optional[Forest[K,V]]
 
-	def check(self:Optional[Forest[K,V]], key:K) -> int:
+	def __new__(cls, order:int, tree:Tree[K,V], next:Optional[Forest[K,V]]):
+		self = super().__new__(cls)
+		self.order = order
+		self.tree = tree
+		self.next = next
+		return self
+
+	def _check(self:Optional[Forest[K,V]], key:K) -> int:
 		if self is None: return 0
-		tsize = self.tree.check(key, self.order)
-		fsize = Forest.check(self.next, key)
+		tsize = self.tree._check(key, self.order)
+		fsize = Forest._check(self.next, key)
 		return tsize + fsize
 
 	def insert(self:Optional[Forest[K,V]], order:int, tree:Tree[K,V]) -> Forest[K,V]:
@@ -88,12 +110,12 @@ class Forest(Generic[K,V]):
 		self.tree.pprint(indent + 1)
 		Forest.pprint(self.next, indent)
 
-	def extract(self:Forest[K,V]) \
-			-> tuple[int,Optional[Branch[K,V]],K,V,Optional[Forest[K,V]]]:
+	def _extract(self:Forest[K,V]) \
+			-> Tuple[int,Optional[Branch[K,V]],K,V,Optional[Forest[K,V]]]:
 		if self.next is None:
 			return self.order - 1, self.tree.branch, \
 				self.tree.key, self.tree.value, None
-		order, branch, key, value, forest = self.next.extract()
+		order, branch, key, value, forest = self.next._extract()
 		if self.tree.key < key:
 			return self.order - 1, self.tree.branch, \
 				self.tree.key, self.tree.value, self.next
@@ -104,8 +126,8 @@ class Forest(Generic[K,V]):
 		return order, branch, key, value, \
 			Forest.insert(forest, self.order, self.tree)
 
-	def extract1(self:Forest[K,V]) -> tuple[K,V,Optional[Forest[K,V]]]:
-		order, branch, key, value, forest = self.extract()
+	def extract(self:Forest[K,V]) -> Tuple[K,V,Optional[Forest[K,V]]]:
+		order, branch, key, value, forest = self._extract()
 		while branch is not None:
 			forest = Forest.insert(forest, order, branch.tree)
 			order, branch = order - 1, branch.next
@@ -126,9 +148,16 @@ class Forest(Generic[K,V]):
 			self.tree.merge(other.tree))
 
 class QueueView(Generic[T,K,V], Collection[T]):
-	def __init__(self, queue:Queue[K,V], sorted:bool):
+	__slots__ = ('queue', 'sorted')
+
+	queue: Queue[K,V]
+	sorted: bool
+
+	def __new__(cls, queue:Queue[K,V], sorted:bool):
+		self = super().__new__(cls)
 		self.queue = queue
 		self.sorted = sorted
+		return self
 
 	def __contains__(self, item):
 		return any(item == i for i in self)
@@ -136,7 +165,7 @@ class QueueView(Generic[T,K,V], Collection[T]):
 	def __len__(self):
 		return self.queue.size
 
-	def _iter_unsorted(self) -> Iterator[tuple[K,V]]:
+	def _iter_unsorted(self) -> Iterator[Tuple[K,V]]:
 		if self.queue.size == 0: return
 		yield self.queue.key, self.queue.value
 		forest = self.queue.forest
@@ -155,23 +184,23 @@ class QueueView(Generic[T,K,V], Collection[T]):
 					stack.append(item.tree)
 			forest = forest.next
 
-	def _iter_sorted(self) -> Iterator[tuple[K,V]]:
+	def _iter_sorted(self) -> Iterator[Tuple[K,V]]:
 		queue = self.queue
 		while queue.size > 0:
 			key, value, queue = queue.extract()
 			yield key, value
 
-	def _iter(self) -> Iterator[tuple[K,V]]:
+	def _iter(self) -> Iterator[Tuple[K,V]]:
 		if self.sorted:
 			return self._iter_sorted()
 		return self._iter_unsorted()
 
 	@abstractmethod
 	def __iter__(self) -> Iterator[T]:
-		pass
+		return NotImplemented
 
-class QueueItems(QueueView[tuple[K,V],K,V]):
-	def __iter__(self) -> Iterator[tuple[K,V]]:
+class QueueItems(QueueView[Tuple[K,V],K,V]):
+	def __iter__(self) -> Iterator[Tuple[K,V]]:
 		return super()._iter()
 
 class QueueKeys(QueueView[K,K,V]):
@@ -182,56 +211,69 @@ class QueueValues(QueueView[V,K,V]):
 	def __iter__(self) -> Iterator[V]:
 		return (v for k, v in super()._iter())
 
-@dataclass(slots=True, frozen=True)
 class Queue(Generic[K,V]):
+	__slots__ = ('down', 'size', 'key', 'value', 'forest')
+
+	down: bool
 	size: int
 	key: K
 	value: V
 	forest: Optional[Forest[K,V]]
 
-	def check(self):
+	def __new__(cls, down:bool, size:int,
+			key:K, value:V, forest:Optional[Forest[K,V]]):
+		self = super().__new__(cls)
+		self.down = down
+		self.size = size
+		self.key = key
+		self.value = value
+		self.forest = forest
+		return self
+
+	def _check(self):
 		if self.size == 0:
 			assert self.key is None
 			assert self.value is None
 			assert self.forest is None
 		else:
-			size = Forest.check(self.forest, self.key)
+			size = Forest._check(self.forest, self.key)
 			assert self.size == size + 1
-
-	@staticmethod
-	def empty() -> Queue[Any,Any]:
-		return Queue(0, None, None, None)
 
 	def insert(self, key:K, value:V=cast(V,None)) -> Queue[K,V]:
 		if self.size == 0:
-			return Queue(1, key, value, None)
+			return Queue(False, 1, key, value, None)
 		if key < self.key:
 			k1, v1, k2, v2 = key, value, self.key, self.value
 		else:
 			k2, v2, k1, v1 = key, value, self.key, self.value
-		return Queue(self.size + 1, k1, v1,
+		return Queue(False, self.size + 1, k1, v1,
 			Forest.insert(self.forest, 0, Tree(k2, v2, None)))
 
 	def _pprint(self):
 		print('Queue', self.size, self.key, self.value)
 		Forest.pprint(self.forest, 0)
 
-	def extract(self) -> tuple[K,V,Queue[K,V]]:
+	def extract(self) -> Tuple[K,V,Queue[K,V]]:
 		if self.size == 0:
 			raise IndexError('extract from empty queue')
 		if self.forest is None:
-			return self.key, self.value, Queue.empty()
-		key, value, forest = self.forest.extract1()
-		return self.key, self.value, Queue(self.size - 1, key, value, forest)
+			return self.key, self.value, _EMPTY_QUEUE
+		key, value, forest = self.forest.extract()
+		return self.key, self.value, Queue(False, self.size - 1, key, value, forest)
 
-	def merge(self, other:Queue[K,V]) -> Queue[K,V]:
+	def _merge(self, other:Queue[K,V]) -> Queue[K,V]:
 		if self.size == 0: return other
 		if other.size == 0: return self
 		if self.key >= other.key:
 			self, other = other, self
 		forest = Forest.merge(self.forest, other.forest)
 		forest = Forest.insert(forest, 0, Tree(other.key, other.value, None))
-		return Queue(self.size + other.size, self.key, self.value, forest)
+		return Queue(False, self.size + other.size, self.key, self.value, forest)
+
+	def merge(self, other:Union[Queue[K,V],Iterable[Tuple[K,V]]]) -> Queue[K,V]:
+		return self._merge(queue(other))
+
+	__add__ = merge
 
 	def peek(self):
 		if self.size == 0:
@@ -241,15 +283,11 @@ class Queue(Generic[K,V]):
 	def __len__(self):
 		return self.size
 
-	def __add__(self, other:Union[Queue[K,V],Iterable[tuple[K,V]]]) -> Queue[K,V]:
-		if isinstance(other, Queue):
-			return self.merge(other)
-		for key, value in other:
-			self = self.insert(key, value)
-		return self
-
 	def __contains__(self, key):
 		return key in QueueKeys(self, False)
+
+	def __repr__(self):
+		return 'queue({})'.format(list(self.items()))
 
 	def items(self, sorted=True):
 		return QueueItems(self, sorted)
@@ -260,9 +298,9 @@ class Queue(Generic[K,V]):
 	def values(self, sorted=True):
 		return QueueValues(self, sorted)
 
-def queue(items:Optional[Iterable[tuple[K,V]]]=None) -> Queue[K,V]:
-	if items is None:
-		return Queue.empty()
+_EMPTY_QUEUE:Queue[Any,Any] = Queue(False, 0, None, None, None)
+
+def queue(items:Union[Queue[K,V],Iterable[Tuple[K,V]]]=_EMPTY_QUEUE) -> Queue[K,V]:
 	if isinstance(items, Queue):
 		return items
 	size, forest = 0, None
@@ -270,14 +308,11 @@ def queue(items:Optional[Iterable[tuple[K,V]]]=None) -> Queue[K,V]:
 		forest = Forest.insert(forest, 0, Tree(key, value, None))
 		size += 1
 	if forest is None:
-		return Queue.empty()
-	key, value, forest = forest.extract1()
-	return Queue(size, key, value, forest)
+		return _EMPTY_QUEUE
+	key, value, forest = forest.extract()
+	return Queue(False, size, key, value, forest)
 
-def q(*items:tuple[K,V]) -> Queue[K,V]:
+def q(*items:Tuple[K,V]) -> Queue[K,V]:
 	return queue(items)
 
-Collection.register(Queue)
-
 __all__ = ('q', 'queue', 'Queue')
-
