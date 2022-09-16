359639f8dba4613b1878d6c6a86b462d6b4d08ab
mingmingrr <mingmingrr@gmail.com>
2022-09-16 11:47:20 -0400

    heap: add compare and merge Tree with Branch

--- pyrsistent_extras/pheap.py
+++ pyrsistent_extras/pheap.py
@@ -1,78 +1,43 @@
 from __future__ import annotations
 
-from typing import Collection, Iterable, Iterator, \
+from typing import Collection, Iterable, Iterator, Hashable, \
 	ClassVar, TypeVar, Generic, Optional, Callable, Tuple, Any, Union, cast
 from abc import abstractmethod
 
-from .util import Comparable
+import itertools
+import operator
+
+from .util import Comparable, compare_single, compare_next, compare_iter
 
 T = TypeVar('T')
 K = TypeVar('K', bound=Comparable)
 V = TypeVar('V')
 
-class Branch(Generic[K,V]):
-	__slots__ = ('_tree', '_next')
-
-	_tree: Tree[K,V]
-	_next: Optional[Branch[K,V]]
-
-	def __new__(cls, tree:Tree[K,V], next:Optional[Branch[K,V]]):
-		self = super().__new__(cls)
-		self._tree = tree
-		self._next = next
-		return self
-
-	def _check(self:Optional[Branch[K,V]], key:K, order:int) -> int:
-		if self is None:
-			assert order == -1
-			return 0
-		else:
-			tsize = self._tree._check(key, order)
-			bsize = Branch._check(self._next, key, order - 1)
-			return tsize + bsize
-
-	def pprint(self:Optional[Branch[K,V]], indent:int):
-		if self is None: return
-		self._tree.pprint(indent)
-		Branch.pprint(self._next, indent)
-
 class Tree(Generic[K,V]):
-	__slots__ = ('_key', '_value', '_branch')
+	__slots__ = ('_key', '_value', '_child', '_sibling')
 
 	_key: K
 	_value: V
-	_branch: Optional[Branch[K,V]]
+	_child: Optional[Tree[K,V]]
+	_sibling: Optional[Tree[K,V]]
 
-	def __new__(cls, key:K, value:V, branch:Optional[Branch[K,V]]):
+	def __new__(cls, key:K, value:V,
+			child:Optional[Tree[K,V]], sibling:Optional[Tree[K,V]]):
 		self = super().__new__(cls)
 		self._key = key
 		self._value = value
-		self._branch = branch
+		self._child = child
+		self._sibling = sibling
 		return self
-
-	def _check(self, key:K, order:int) -> int:
-		assert self._key >= key
-		assert self.order() == order
-		size = Branch._check(self._branch, self._key, order - 1)
-		return size + 1
 
 	def merge(self, other:Optional[Tree[K,V]], down:bool) -> Tree[K,V]:
 		if other is None: return self
 		if (self._key < other._key) == down:
 			self, other = other, self
-		return Tree(self._key, self._value,
-			Branch(other, self._branch))
-
-	def pprint(self:Optional[Tree[K,V]], indent:int):
-		if self is None: return
-		print('  ' * indent, 'Tree', self._key, self._value)
-		Branch.pprint(self._branch, indent + 1)
-
-	def order(self) -> int:
-		order, branch = 0, self._branch
-		while branch is not None:
-			order, branch = order + 1, branch._next
-		return order
+		assert self._sibling is None
+		assert other._sibling is None
+		other = Tree(other._key, other._value, other._child, self._child)
+		return Tree(self._key, self._value, other, None)
 
 class Forest(Generic[K,V]):
 	__slots__ = ('_order', '_tree', '_next')
@@ -88,13 +53,7 @@ class Forest(Generic[K,V]):
 		self._next = next
 		return self
 
-	def _check(self:Optional[Forest[K,V]], key:K) -> int:
-		if self is None: return 0
-		tsize = self._tree._check(key, self._order)
-		fsize = Forest._check(self._next, key)
-		return tsize + fsize
-
-	def insert(self:Optional[Forest[K,V]], down:bool,
+	def push(self:Optional[Forest[K,V]], down:bool,
 			order:int, tree:Tree[K,V]) -> Forest[K,V]:
 		if self is None:
 			return Forest(order, tree, None)
@@ -102,37 +61,34 @@ class Forest(Generic[K,V]):
 			return Forest(order, tree, self)
 		if order > self._order:
 			return Forest(self._order, self._tree,
-				Forest.insert(self._next, down, order, tree))
-		return Forest.insert(self._next, down, order + 1,
+				Forest.push(self._next, down, order, tree))
+		return Forest.push(self._next, down, order + 1,
 			self._tree.merge(tree, down))
 
-	def pprint(self:Optional[Forest[K,V]], indent:int):
-		if self is None: return
-		print('  ' * indent, f'Forest r{self._order}')
-		self._tree.pprint(indent + 1)
-		Forest.pprint(self._next, indent)
-
-	def _extract(self:Forest[K,V], down:bool) \
-			-> Tuple[int,Optional[Branch[K,V]],K,V,Optional[Forest[K,V]]]:
+	def popbranch(self:Forest[K,V], down:bool) \
+			-> Tuple[int,Optional[Tree[K,V]],K,V,Optional[Forest[K,V]]]:
+		assert self._tree._sibling is None
 		if self._next is None:
-			return self._order - 1, self._tree._branch, \
+			return self._order - 1, self._tree._child, \
 				self._tree._key, self._tree._value, None
-		order, branch, key, value, forest = self._next._extract(down)
+		order, branch, key, value, forest = self._next.popbranch(down)
 		if (self._tree._key < key) != down:
-			return self._order - 1, self._tree._branch, \
+			return self._order - 1, self._tree._child, \
 				self._tree._key, self._tree._value, self._next
 		while order >= self._order:
 			assert branch is not None
-			forest = Forest.insert(forest, down, order, branch._tree)
-			order, branch = order - 1, branch._next
+			tree = Tree(branch._key, branch._value, branch._child, None)
+			forest = Forest.push(forest, down, order, tree)
+			order, branch = order - 1, branch._sibling
 		return order, branch, key, value, \
-			Forest.insert(forest, down, self._order, self._tree)
+			Forest.push(forest, down, self._order, self._tree)
 
-	def extract(self:Forest[K,V], down:bool) -> Tuple[K,V,Optional[Forest[K,V]]]:
-		order, branch, key, value, forest = self._extract(down)
+	def pop(self:Forest[K,V], down:bool) -> Tuple[K,V,Optional[Forest[K,V]]]:
+		order, branch, key, value, forest = self.popbranch(down)
 		while branch is not None:
-			forest = Forest.insert(forest, down, order, branch._tree)
-			order, branch = order - 1, branch._next
+			tree = Tree(branch._key, branch._value, branch._child, None)
+			forest = Forest.push(forest, down, order, tree)
+			order, branch = order - 1, branch._sibling
 		return key, value, forest
 
 	def merge(self:Optional[Forest[K,V]], other:Optional[Forest[K,V]],
@@ -146,7 +102,7 @@ class Forest(Generic[K,V]):
 			return Forest(other._order, other._tree,
 				Forest.merge(other._next, self, down))
 		forest = Forest.merge(self._next, other._next, down)
-		return Forest.insert(forest, down, self._order + 1,
+		return Forest.push(forest, down, self._order + 1,
 			self._tree.merge(other._tree, down))
 
 class HeapView(Generic[T,K,V], Collection[T]):
@@ -171,25 +127,23 @@ class HeapView(Generic[T,K,V], Collection[T]):
 		if self._queue._size == 0: return
 		yield self._queue._key, self._queue._value
 		forest = self._queue._forest
-		stack:list[Union[Branch[K,V],Tree[K,V]]] = []
+		stack:list[Tree[K,V]] = []
 		while forest is not None:
 			stack.append(forest._tree)
 			while stack:
 				item = stack.pop()
 				if isinstance(item, Tree):
 					yield item._key, item._value
-					if item._branch is not None:
-						stack.append(item._branch)
-				else:
-					if item._next is not None:
-						stack.append(item._next)
-					stack.append(item._tree)
+					if item._sibling is not None:
+						stack.append(item._sibling)
+					if item._child is not None:
+						stack.append(item._child)
 			forest = forest._next
 
 	def _iter_sorted(self) -> Iterator[Tuple[K,V]]:
 		queue = self._queue
 		while queue._size > 0:
-			key, value, queue = queue.extract()
+			key, value, queue = queue.pop()
 			yield key, value
 
 	def _iter(self) -> Iterator[Tuple[K,V]]:
@@ -198,7 +152,7 @@ class HeapView(Generic[T,K,V], Collection[T]):
 		return self._iter_unsorted()
 
 	@abstractmethod
-	def __iter__(self) -> Iterator[T]:
+	def __iter__(self) -> Iterator[T]: # pragma: no cover
 		return NotImplemented
 
 class HeapItems(HeapView[Tuple[K,V],K,V]):
@@ -213,7 +167,7 @@ class HeapValues(HeapView[V,K,V]):
 	def __iter__(self) -> Iterator[V]:
 		return (v for k, v in super()._iter())
 
-class Heap(Generic[K,V]):
+class Heap(Generic[K,V], Collection[K], Hashable):
 	__slots__ = ('_size', '_key', '_value', '_forest')
 
 	_size: int
@@ -221,6 +175,7 @@ class Heap(Generic[K,V]):
 	_value: V
 	_forest: Optional[Forest[K,V]]
 
+	_name: ClassVar[str] = cast(Any, None)
 	_down: ClassVar[bool] = cast(Any, None)
 	_empty: ClassVar[Heap[Any,Any]] = cast(Any, None)
 
@@ -232,16 +187,7 @@ class Heap(Generic[K,V]):
 		self._forest = forest
 		return self
 
-	def _check(self):
-		if self._size == 0:
-			assert self._key is None
-			assert self._value is None
-			assert self._forest is None
-		else:
-			size = Forest._check(self._forest, self._key)
-			assert self._size == size + 1
-
-	def insert(self, key:K, value:V=cast(V,None)) -> Heap[K,V]:
+	def push(self, key:K, value:V=cast(V,None)) -> Heap[K,V]:
 		if self._size == 0:
 			return type(self)(1, key, value, None)
 		if (key < self._key) != self._down:
@@ -249,32 +195,26 @@ class Heap(Generic[K,V]):
 		else:
 			k2, v2, k1, v1 = key, value, self._key, self._value
 		return type(self)(self._size + 1, k1, v1,
-			Forest.insert(self._forest, self._down, 0, Tree(k2, v2, None)))
+			Forest.push(self._forest, self._down, 0, Tree(k2, v2, None, None)))
 
-	def _pprint(self):
-		print('Heap', self._size, self._key, self._value)
-		Forest.pprint(self._forest, 0)
-
-	def extract(self) -> Tuple[K,V,Heap[K,V]]:
+	def pop(self) -> Tuple[K,V,Heap[K,V]]:
 		if self._size == 0:
-			raise IndexError('extract from empty heap')
+			raise IndexError('pop from empty heap')
 		if self._forest is None:
 			return self._key, self._value, self._empty
-		key, value, forest = self._forest.extract(self._down)
+		key, value, forest = self._forest.pop(self._down)
 		return self._key, self._value, type(self)(self._size - 1, key, value, forest)
 
-	def _merge(self, other:Heap[K,V]) -> Heap[K,V]:
+	def merge(self, other:HeapLike[K,V]) -> Heap[K,V]:
+		other = self._fromitems(other)
 		if self._size == 0: return other
 		if other._size == 0: return self
 		if (self._key < other._key) == self._down:
 			self, other = other, self
 		forest = Forest.merge(self._forest, other._forest, self._down)
-		forest = Forest.insert(forest, self._down, 0,
-			Tree(other._key, other._value, None))
+		forest = Forest.push(forest, self._down, 0,
+			Tree(other._key, other._value, None, None))
 		return type(self)(self._size + other._size, self._key, self._value, forest)
-
-	def merge(self, other:HeapLike[K,V]) -> Heap[K,V]:
-		return self._merge(self._fromitems(other))
 
 	__add__ = merge
 
@@ -286,12 +226,14 @@ class Heap(Generic[K,V]):
 	def __len__(self):
 		return self._size
 
+	def __bool__(self):
+		return self._size != 0
+
 	def __contains__(self, key):
 		return key in HeapKeys(self, False)
 
 	def __repr__(self):
-		name = 'maxheap' if self._down else 'minheap'
-		return '{}({})'.format(name, list(self.items()))
+		return '{}({})'.format(self._name, list(self.items()))
 
 	def items(self, sorted:bool=True):
 		return HeapItems(self, sorted)
@@ -302,42 +244,127 @@ class Heap(Generic[K,V]):
 	def values(self, sorted:bool=True):
 		return HeapValues(self, sorted)
 
+	def __iter__(self):
+		return iter(HeapKeys(self))
+
 	@classmethod
 	def _fromitems(cls, items:HeapLike[K,V]) -> Heap[K,V]:
 		if isinstance(items, Heap):
-			if isinstance(items, cls): return items
-			return cls._fromitems(items.items())
+			if items._down == cls._down: return items
+			return cls._fromitems(items.items(sorted=False))
 		size, forest = 0, None
 		for key, value in items:
-			forest = Forest.insert(forest, cls._down, 0, Tree(key, value, None))
+			forest = Forest.push(forest, cls._down, 0,
+				Tree(key, value, None, None))
 			size += 1
 		if forest is None:
 			return cls._empty
-		key, value, forest = forest.extract(cls._down)
+		key, value, forest = forest.pop(cls._down)
 		return cls(size, key, value, forest)
+
+	def _iter_sort_values(self) -> Iterator[Tuple[K,V]]:
+		for k, vs in itertools.groupby(self.items(), key=operator.itemgetter(0)):
+			yield from sorted(vs, key=operator.itemgetter(1))
+
+	def _iter_group_values(self) -> Iterator[Tuple[K,Iterator[Tuple[K,V]]]]:
+		return itertools.groupby(self.items(), key=operator.itemgetter(0))
+
+	def _compare(self, other:Heap[K,V], equality:bool) -> int:
+		if not isinstance(other, Heap):
+			return NotImplemented
+		if self._down != other._down:
+			return NotImplemented
+		if equality:
+			if self is other:
+				return 0
+			if len(self) != len(other):
+				return 1
+		try:
+			return compare_iter(self._iter_sort_values(),
+				other._iter_sort_values(), equality)
+		except TypeError as err:
+			if not equality: raise
+		xiter = self._iter_group_values()
+		yiter = other._iter_group_values()
+		while True:
+			n = compare_next(xiter, yiter)
+			if isinstance(n, int): return n
+			if n[0][0] != n[1][0]: return 1
+			xvalues = [v for _, v in n[0][1]]
+			yvalues = [v for _, v in n[1][1]]
+			if len(xvalues) != len(yvalues): return 1
+			for x in xvalues:
+				try:
+					yvalues.remove(x)
+				except ValueError:
+					return 1
+			assert not yvalues
+
+	def __eq__(self, other):
+		result = self._compare(other, True)
+		if result is NotImplemented: return result
+		return result == 0
+	def __ne__(self, other):
+		result = self._compare(other, True)
+		if result is NotImplemented: return result
+		return result != 0
+	def __gt__(self, other):
+		result = self._compare(other, False)
+		if result is NotImplemented: return result
+		return result > 0
+	def __ge__(self, other):
+		result = self._compare(other, False)
+		if result is NotImplemented: return result
+		return result >= 0
+	def __lt__(self, other):
+		result = self._compare(other, False)
+		if result is NotImplemented: return result
+		return result < 0
+	def __le__(self, other):
+		result = self._compare(other, False)
+		if result is NotImplemented: return result
+		return result <= 0
+
+	def __hash__(self):
+		h = hash(self._name)
+		for k, vs in itertools.groupby(self.items(), key=operator.itemgetter(0)):
+			h = hash((h, k, tuple(sorted(vs))))
+		return h
+
+	def __reduce__(self):
+		return globals()[self._name], (tuple(self.items(False)),)
 
 HeapLike = Union[Heap[K,V],Iterable[Tuple[K,V]]]
 
-class MinHeap(Heap[K,V]):
-	_down: ClassVar[bool] = False
-	_empty: ClassVar[MinHeap[Any,Any]] = cast(Any, None)
-MinHeap._empty = MinHeap(0, None, None, None) # type: ignore
+def fromkeys(func):
+	def inner(items:Iterable[K]) -> Heap[K,None]:
+		return func((item, None) for item in items)
+	return inner
 
-def minheap(items:HeapLike[K,V]=MinHeap._empty) -> Heap[K,V]:
-	return MinHeap._fromitems(items)
+class PMinHeap(Heap[K,V]):
+	_name: ClassVar[str] = 'pminheap'
+	_down: ClassVar[bool] = False
+	_empty: ClassVar[PMinHeap[Any,Any]] = cast(Any, None)
+PMinHeap._empty = PMinHeap(0, None, None, None) # type: ignore
+
+def pminheap(items:HeapLike[K,V]=PMinHeap._empty) -> Heap[K,V]:
+	return PMinHeap._fromitems(items)
+setattr(pminheap, 'fromkeys', fromkeys(pminheap))
 
 def hl(*items:Tuple[K,V]) -> Heap[K,V]:
-	return minheap(items)
+	return pminheap(items)
 
-class MaxHeap(Heap[K,V]):
+class PMaxHeap(Heap[K,V]):
+	_name: ClassVar[str] = 'pmaxheap'
 	_down: ClassVar[bool] = True
-	_empty: ClassVar[MaxHeap[Any,Any]] = cast(Any, None)
-MaxHeap._empty = MaxHeap(0, None, None, None) # type: ignore
+	_empty: ClassVar[PMaxHeap[Any,Any]] = cast(Any, None)
+PMaxHeap._empty = PMaxHeap(0, None, None, None) # type: ignore
 
-def maxheap(items:HeapLike[K,V]=MaxHeap._empty) -> Heap[K,V]:
-	return MaxHeap._fromitems(items)
+def pmaxheap(items:HeapLike[K,V]=PMaxHeap._empty) -> Heap[K,V]:
+	return PMaxHeap._fromitems(items)
+setattr(pmaxheap, 'fromkeys', fromkeys(pmaxheap))
 
 def hg(*items:Tuple[K,V]) -> Heap[K,V]:
-	return minheap(items)
+	return pmaxheap(items)
 
-__all__ = ('hl', 'minheap', 'MinHeap', 'hg', 'maxheap', 'MaxHeap')
+__all__ = ('hl', 'pminheap', 'PMinHeap', 'hg', 'pmaxheap', 'PMaxHeap')
---
+++ tests/pheap_test.py
@@ -0,0 +1,224 @@
+from pyrsistent_extras.pheap import \
+	hl, pminheap, PMinHeap, hg, pmaxheap, PMaxHeap
+
+import hypothesis
+from hypothesis import given, strategies as st
+
+import pickle
+import pytest
+import bisect
+
+def trace(*args, **kwargs):
+	print(*args, **kwargs)
+	return args[-1]
+
+def pitems(keys=st.integers(), values=st.integers()):
+	return st.tuples(keys, values)
+
+def makeheaps(funcs):
+	def inner(unique=False, keys=st.integers(), values=st.integers(), **kwargs):
+		if unique: # pragma: no cover
+			kwargs['unique'] = True
+			kwargs['unique_by'] = lambda xs: xs[0]
+		lists = st.lists(pitems(keys, values), **kwargs)
+		return st.builds(lambda f, xs: (f(xs), list(sorted(xs))), funcs, lists)
+	return inner
+
+pmaxheaps = makeheaps(st.just(pmaxheap))
+pminheaps = makeheaps(st.just(pminheap))
+pheaps = makeheaps(st.one_of(st.just(pminheap), st.just(pmaxheap)))
+
+def pheappairs(heaps):
+	return st.one_of(st.tuples(heaps(pminheaps), heaps(pminheaps)),
+		st.tuples(heaps(pmaxheaps), heaps(pmaxheaps)))
+
+class PrettyHeap: # pragma: no cover
+	def __init__(self, heap):
+		self.heap = heap
+	@staticmethod
+	def tree(tree, depth, acc):
+		if tree is None: return
+		acc.append('{}Tree {} {}'.format('  ' * depth, tree._key, tree._value))
+		PrettyHeap.tree(tree._child, depth + 1, acc)
+		PrettyHeap.tree(tree._sibling, depth, acc)
+	@staticmethod
+	def forest(forest, acc):
+		if forest is None: return
+		acc.append('  Forest {}'.format(forest._order))
+		PrettyHeap.tree(forest._tree, 2, acc)
+		PrettyHeap.forest(forest._next, acc)
+	def __repr__(self):
+		acc = ['{} {} {} {}'.format(
+			type(self.heap).__name__,
+			self.heap._size,
+			self.heap._key,
+			self.heap._value)]
+		PrettyHeap.forest(self.heap._forest, acc)
+		return '\n'.join(acc)
+
+def check_tree(tree, down, tip, order, acc):
+	'''
+	check invariants of Tree
+	'''
+	if tree is None: return 0
+	if down: assert tree._key <= tip
+	else: assert tree._key >= tip
+	acc.append((tree._key, tree._value))
+	if order == 0:
+		assert tree._child is None
+		assert tree._sibling is None
+	else:
+		assert tree._child is not None
+	size1 = check_tree(tree._child, down, tree._key, order - 1, acc)
+	size2 = check_tree(tree._sibling, down, tip, order - 1, acc)
+	return size1 + size2 + 1
+
+def check_forest(forest, down, tip, acc):
+	'''
+	check invariants of Forest
+	'''
+	if forest is None: return 0
+	assert forest._tree._sibling is None
+	size = check_tree(forest._tree, down, tip, forest._order, acc)
+	assert size == (1 << forest._order)
+	size += check_forest(forest._next, down, tip, acc)
+	return size
+
+def check_heap(heap):
+	'''
+	check invariants of Heap
+	'''
+	assert isinstance(heap, (PMinHeap, PMaxHeap))
+	assert heap._down == isinstance(heap, PMaxHeap)
+	if heap._size == 0:
+		assert heap._key is None
+		assert heap._value is None
+		assert heap._forest is None
+		return []
+	acc = [(heap._key, heap._value)]
+	size = check_forest(heap._forest, heap._down, heap._key, acc)
+	assert size + 1 == heap._size
+	return list(sorted(acc))
+
+@given(pheaps(min_size=20))
+def test_types(heapitems):
+	heap, items = heapitems
+	assert isinstance(heap, (PMinHeap, PMaxHeap))
+	assert type(items) is list
+	if items:
+		key, value = items[0]
+		assert type(key) is int
+		assert type(value) is int
+	assert check_heap(heap) == items
+
+@given(st.lists(pitems()))
+def test_hl_hg(items):
+	assert check_heap(hg(*items)) == check_heap(pmaxheap(items))
+	assert check_heap(hl(*items)) == check_heap(pminheap(items))
+
+@given(pheaps())
+def test_fromkeys(heapitems):
+	heap, items = heapitems
+	fromkeys = globals()[heap._name].fromkeys
+	xs = check_heap(fromkeys(k for k, _ in items))
+	assert xs == [(k, None) for k, _ in items]
+
+@given(pheaps(), st.integers(), st.integers())
+def test_push(heapitems, key, value):
+	heap, items = heapitems
+	bisect.insort(items, (key, value))
+	assert check_heap(heap.push(key, value)) == items
+
+@given(pheaps(), st.integers(), st.integers())
+def test_pop(heapitems, key, value):
+	heap, items = heapitems
+	if items:
+		key1, value1, heap1 = heap.pop()
+		if isinstance(heap, PMaxHeap):
+			key2, value2 = items[-1]
+			items2 = items[:-1]
+		else:
+			key2, value2 = items[0]
+			items2 = items[1:]
+		assert key1 == key2
+		assert [k for k, _ in check_heap(heap1)] == [k for k, _ in items2]
+	else:
+		with pytest.raises(IndexError):
+			heap.pop()
+
+@given(pheappairs(lambda heaps: heaps()))
+def test_merge(args):
+	heapitems1, heapitems2 = args
+	heap1, items1 = heapitems1
+	heap2, items2 = heapitems2
+	items = list(sorted(items1 + items2))
+	assert check_heap(heap1.merge(heap2)) == items
+	assert check_heap(heap1 + heap2) == items
+
+@given(pheaps(), st.integers())
+def test_contains(heapitems, key):
+	heap, items = heapitems
+	assert (key in heap) == any(k == key for k, v in items)
+
+@given(pheaps())
+def test_len(heapitems):
+	heap, items = heapitems
+	assert len(heap) == len(items)
+
+@given(pheappairs(lambda heaps:
+	heaps(keys=st.integers(1,4), values=st.just(0))))
+def test_compare_int(args):
+	heapitems1, heapitems2 = args
+	heap1, items1 = heapitems1
+	heap2, items2 = heapitems2
+	if heap1._down:
+		assert heap2._down
+		items1 = list(reversed(items1))
+		items2 = list(reversed(items2))
+	assert heap1 == heap1 ; assert heap2 == heap2
+	assert (heap1 == heap2) == (items1 == items2)
+	assert (heap1 != heap2) == (items1 != items2)
+	assert (heap1 <= heap2) == (items1 <= items2)
+	assert (heap1 >= heap2) == (items1 >= items2)
+	assert (heap1 <  heap2) == (items1 <  items2)
+	assert (heap1 >  heap2) == (items1 >  items2)
+
+@given(pheappairs(lambda heaps:
+	heaps(keys=st.integers(1,4), values=st.just(None))))
+def test_compare_none(args):
+	heapitems1, heapitems2 = args
+	heap1, items1 = heapitems1
+	heap2, items2 = heapitems2
+	assert heap1 == heap1 ; assert heap2 == heap2
+	assert (heap1 == heap2) == (items1 == items2)
+	assert (heap1 != heap2) == (items1 != items2)
+
+smallitems = pitems(keys=st.integers(1,4), values=st.integers(1,4))
+@given(st.lists(smallitems), st.lists(smallitems))
+def test_compare_dict(items1, items2):
+	items1 = [(k, {v:None}) for k, v in sorted(items1)]
+	items2 = [(k, {v:None}) for k, v in sorted(items2)]
+	heap1, heap2 = pminheap(items1), pminheap(items2)
+	assert heap1 == heap1 ; assert heap2 == heap2
+	assert (heap1 == heap2) == (items1 == items2)
+	assert (heap1 != heap2) == (items1 != items2)
+
+@given(pheaps())
+def test_hash(heapitems):
+	heap1, items = heapitems
+	heap2 = globals()[heap1._name](reversed(items))
+	assert hash(heap1) == hash(heap2)
+
+@given(pheaps())
+def test_repr(heapitems):
+	heap1, items = heapitems
+	heap2 = eval(repr(heap1))
+	assert type(heap1) == type(heap2)
+	assert check_heap(heap2) == sorted(items)
+
+@given(pheaps())
+def test_reduce(heapitems):
+	heap, items = heapitems
+	assert check_heap(pickle.loads(pickle.dumps(heap))) == sorted(items)
+
+# vim: set foldmethod=marker:
