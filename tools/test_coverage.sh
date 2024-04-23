ef99b28877eaceef2a741c07dada1ba784f2e5d2
mingmingrr <mingmingrr@gmail.com>
2024-04-23 17:31:29 -0400

    coverage: add dependencies

--- tools/test_coverage.sh
+++ tools/test_coverage.sh
@@ -4,10 +4,12 @@ set -e
 
 export SKBUILD_CMAKE_BUILD_TYPE=Debug
 export SKBUILD_BUILD_DIR=build/coverage
-export HYPOTHESIS_PROFILE=coverage
+export HYPOTHESIS_PROFILE=${HYPOTHESIS_PROFILE:-coverage}
 
-pip install --no-build-isolation --editable '.[test]'
+pip install --no-build-isolation --editable '.[test,coverage]'
 coverage run -m pytest -vv
+
+coverage html --directory .py-coverage
 
 lcov --capture --directory $SKBUILD_BUILD_DIR --output-file $SKBUILD_BUILD_DIR/coverage.info
 genhtml $SKBUILD_BUILD_DIR/coverage.info --output-directory .covreport
