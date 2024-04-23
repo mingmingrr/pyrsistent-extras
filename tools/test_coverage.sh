4739eb0c2ffa7239f7e5ff9a3a1f6c823d5a0100
mingmingrr <mingmingrr@gmail.com>
2024-04-23 17:31:48 -0400

    coverage: merge with python coverage

--- tools/test_coverage.sh
+++ tools/test_coverage.sh
@@ -12,4 +12,7 @@ coverage run -m pytest -vv
 coverage html --directory .py-coverage
 
 lcov --capture --directory $SKBUILD_BUILD_DIR --output-file $SKBUILD_BUILD_DIR/coverage.info
-genhtml $SKBUILD_BUILD_DIR/coverage.info --output-directory .covreport
+genhtml $SKBUILD_BUILD_DIR/coverage.info --output-directory .cpp-coverage
+
+cpp-coveralls --root $SKBUILD_BUILD_DIR --dump $SKBUILD_BUILD_DIR/cpp-coveralls.json
+python3 -m coveralls --merge $SKBUILD_BUILD_DIR/cpp-coveralls.json --output coveralls.json
